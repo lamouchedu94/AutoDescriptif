f85f5ee564e8d9faf3f2e46ce67dd9eec6aa02cc
lamouchedu94 <lamouchedu54@gmail.com>
2022-06-12 14:50:05 +0200

    Gestion du profile cpu et de la limite en parametres

--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module main.go
+module autodescriptif
 
 go 1.18
--- main.go
+++ main.go
@@ -1,16 +1,45 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
+	"os"
+	"runtime/pprof"
 	"sort"
 	"strconv"
 	"sync"
 	"time"
 )
 
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile to `file`")
+
 func main() {
-	th := 1
+	var err error
 	nb := 3000000
+	flag.Parse()
+	if flag.NArg() > 0 {
+		nb, err = strconv.Atoi(flag.Arg(0))
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+	}
+
+	if *cpuprofile != "" {
+		f, err := os.Create(*cpuprofile)
+		if err != nil {
+			log.Fatal("could not create CPU profile: ", err)
+		}
+		defer f.Close() // error handling omitted for example
+		if err := pprof.StartCPUProfile(f); err != nil {
+			log.Fatal("could not start CPU profile: ", err)
+		}
+		defer pprof.StopCPUProfile()
+	}
+
+	th := 1
+
 	lockval := sync.Mutex{}
 
 	val := []int{}
@@ -38,6 +67,7 @@ func main() {
 
 func segment(i int, inter int) []int {
 	a := []int{}
+
 	for j := inter * i; j < inter*(i+1); j++ {
 		if EstAutodesc2(strconv.Itoa(j)) {
 			a = append(a, j)
